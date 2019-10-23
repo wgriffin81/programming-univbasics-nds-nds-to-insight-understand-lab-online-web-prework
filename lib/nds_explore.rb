75aa31d6b472f37e7acb0bf3b3afa45f36c558a1
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-23 04:53:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,5 +8,26 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  
+  row_index = 0
+  while row_index < vm.length do
+    puts "Row #{row_index} has #{vm[row_index]} columns"
+ 
+    column_index = 0
+  while column_index < vm[row_index].length do
+    coord = "#{row_index}, #{column_index}"
+    inner_len = vm[row_index][column_index].length
+    # Remember \t is a TAB character for indentation
+    puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+ 
+    inner_index = 0
+    while inner_index < inner_len do
+      puts "\t\t (#{coord}, #{inner_index}) is: #{vm[row_index][column_index][inner_index]}"
+      inner_index += 1
+    end
+ 
+    column_index += 1
+  end
+ 
+  row_index += 1
+end
 end
